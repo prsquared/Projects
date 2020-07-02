9d2e6bbcbbc601680ee3e1dcab9f0ecd46457cce
Priyesh <priyesh321988@gmail.com>
2020-07-02 19:00:00 -0400

    Testing

--- BankingSystem/src/banking/Main.java
+++ BankingSystem/src/banking/Main.java
@@ -3,8 +3,9 @@ package banking;
 import banking.services.BankingService;
 
 public class Main {
-
+    //test
     public static void main(String[] args) {
+        System.out.println("Testing");
         BankingService service = new BankingService();
         service.start();
     }
